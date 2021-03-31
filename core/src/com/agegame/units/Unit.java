754f90e403412190c80e7bd8a291e33443f66698
HaViXus <tukedarr@gmail.com>
2021-03-31 14:32:27 +0200

    Change Unit from Interface to Abstract Class. Unit has default behaviours. Move part of methods to Unit class

--- core/src/com/agegame/units/Unit.java
+++ core/src/com/agegame/units/Unit.java
@@ -1,44 +1,238 @@
 package com.agegame.units;
 
 import com.agegame.Direction;
+import com.agegame.health_bar.UnitHealthBar;
 import com.agegame.map.Map;
+import com.agegame.map.MapLine;
 import com.agegame.player.Action;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 import java.util.ArrayList;
 
-public interface Unit {
-    enum UnitState {MOVE, FIGHT, KNOCK_BACKED, WOUNDED, DEAD};
+public abstract class Unit extends Actor{
+    public enum UnitState {MOVE, FIGHT, KNOCK_BACKED, WOUNDED, DEAD};
+    protected Map map;
+    protected Vector2 position;
+    protected Direction.direction direction;
+    protected Action.DomainType domain;
+    protected float speed;
+    protected UnitState state;
+    protected Texture texture;
+    protected Pixmap pixmap;
+
+    protected ArrayList<Rectangle> hitboxes;
+    protected ArrayList<Rectangle> fieldOfView;
+    protected ArrayList<DamageField> damageFields;
+    protected float actionTimer;
+    protected boolean damageFieldCreated;
+    protected UnitHealthBar healthBar;
+
+    protected float HP;
+    protected float damage;
+    protected float knockback;
+    protected float knockbackProtection;
+
+    protected boolean isDead;
+
+    protected ArrayList<Unit> enemyUnitsInRange;
+
+    protected float takenDamage;
+    protected float takenKnockback;
     
-    void init(Map map, Vector2 startPosition, Direction.direction direction, Action.DomainType domain);
+    public void init(Map map, Vector2 startPosition, Direction.direction direction, Action.DomainType domain){
+        this.map = map;
+        this.direction = direction;
+        this.domain = domain;
+        position = startPosition;
+        state = UnitState.MOVE;
+        actionTimer = 0;
+        damageFieldCreated = false;
+        isDead = false;
+        enemyUnitsInRange = new ArrayList<>();
+        damageFields = new ArrayList<>();
+    }
 
-    void update(float delta);
+    public void update(float delta){
+        actionTimer += delta;
+        updateEnemiesInRange();
+        handleTakenDamage();
+        updateState();
+        handleState(delta);
 
-    void move(float delta);
+        healthBar.update(HP, delta);
+    }
 
-    void fight();
+    abstract public void move(float delta);
 
-    void damage(float damage, float knockback);
+    abstract public void fight();
 
-    void dispose();
+    public void damage(float damage, float knockback) {
+        takenDamage += damage;
+        takenKnockback += knockback;
+    }
 
-    Actor getActor();
+    protected void initHitboxes(){
+        hitboxes = new ArrayList<>();
+        Rectangle hitbox = new Rectangle(position.x, position.y, getWidth(), getHeight());
+        hitboxes.add(hitbox);
+    }
 
-    Action.DomainType getDomain();
+    protected void initFieldOfView(){
+        fieldOfView = new ArrayList<>();
+        float startX = position.x + getWidth() / 2 - direction.getIntValue() * getWidth() / 2;
+        int fieldWidth = 50;
+        int fieldHeight = 30;
+        if(direction == Direction.direction.RIGHT) startX -= fieldWidth;
+        Rectangle viewHitbox = new Rectangle(startX, position.y, fieldWidth, fieldHeight);
+        fieldOfView.add(viewHitbox);
+    }
 
-    Vector2 getPosition();
+    protected void initHealthBar(){
+        Vector2 positionCenter = new Vector2(position.x + getWidth()/2, position.y + getHeight() + 5);
+        Vector2 size = new Vector2(getWidth() + 10, 8);
+        healthBar = new UnitHealthBar(positionCenter, size, HP, false);
+    }
 
-    Direction.direction getDirection();
+    protected void updateHealthBarPosition(){
+        healthBar.getPosition().x = position.x + getWidth()/2;
+        healthBar.getPosition().y = position.y + getHeight() + 5;
+    }
 
-    ArrayList<Rectangle> getHitboxes();
+    protected void updateHitboxes(float offset) {
+        for(Rectangle hitbox : hitboxes){
+            hitbox.x += offset;
+        }
+    }
 
-    ArrayList<DamageField> getDamageFields();
+    protected void handleTakenDamage(){
+        HP -= takenDamage;
+        takenDamage = 0;
+        takenKnockback = 0;
+    }
 
-    UnitState getState();
+    protected void updateState(){
+        if(state != UnitState.DEAD)
+        {
+            if(enemyUnitsInRange.size() > 0){
+                changeState(UnitState.FIGHT);
+            }
+            else{
+                changeState(UnitState.MOVE);
+            }
 
-    boolean isDead();
+            if(HP <= 0){
+                changeState(UnitState.DEAD);
+            }
+        }
+    }
+
+    protected void handleState(float delta){
+        switch (state){
+            case MOVE:
+                move(delta);
+                break;
+            case FIGHT:
+                fight();
+                break;
+            case DEAD:
+                death();
+                break;
+        }
+    }
+
+    protected void death(){
+        if(actionTimer > 2){
+            isDead = true;
+        }
+    }
+
+    protected void updateFieldOfView(float offset){
+        for(Rectangle view: fieldOfView){
+            view.x += offset;
+        }
+    }
+
+    protected void changeState(UnitState newState){
+        if(state != newState){
+            state = newState;
+            actionTimer = 0;
+            damageFieldCreated = false;
+        }
+    }
+
+    protected void updateEnemiesInRange(){
+        enemyUnitsInRange.clear();
+
+        MapLine landLine = map.getLines().get(Action.DomainType.LAND_UNIT);
+        for(Unit unit : landLine.units){
+            if(unit != this && unit.getDirection() != direction && unit.getState() != UnitState.DEAD){
+                float distanceX = Math.abs(unit.getPosition().x - position.x);
+                if(distanceX < 100){
+                    Rectangle enemyHitbox = unit.getHitboxes().get(0);
+                    for(Rectangle fieldOfView : fieldOfView){
+                        if(Intersector.overlaps(enemyHitbox, fieldOfView)){
+                            enemyUnitsInRange.add(unit);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(texture, position.x, position.y);
+        healthBar.draw(batch);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    public void dispose() {
+        pixmap.dispose();
+        texture.dispose();
+    }
+
+    public Actor getActor(){
+        return this;
+    }
+
+    public Action.DomainType getDomain() {
+        return domain;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public Direction.direction getDirection() {
+        return direction;
+    }
+
+    public ArrayList<Rectangle> getHitboxes() {
+        return hitboxes;
+    }
+
+    public ArrayList<DamageField> getDamageFields() {
+        return damageFields;
+    }
+
+    public UnitState getState() {
+        return state;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
 
 
 }
--- core/src/com/agegame/units/land_units/UgaBuga.java
+++ core/src/com/agegame/units/land_units/UgaBuga.java
@@ -1,9 +1,7 @@
 package com.agegame.units.land_units;
 
 import com.agegame.Direction;
-import com.agegame.health_bar.UnitHealthBar;
 import com.agegame.map.Map;
-import com.agegame.map.MapLine;
 import com.agegame.player.Action;
 import com.agegame.units.DamageField;
 import com.agegame.units.Unit;
@@ -11,40 +9,10 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 
-import java.util.ArrayList;
 
-public class UgaBuga extends Actor implements Unit {
-    private Map map;
-    private Vector2 position;
-    private Direction.direction direction;
-    private Action.DomainType domain;
-    private float speed;
-    private UnitState state;
-    private Texture texture;
-    private Pixmap pixmap;
-    private ArrayList<Rectangle> hitboxes;
-    private ArrayList<Rectangle> fieldOfView;
-    private ArrayList<DamageField> damageFields;
-    private float actionTimer;
-    private boolean damageFieldCreated;
-    private UnitHealthBar healthBar;
-
-    private float HP;
-    private float damage;
-    private float knockback;
-    private float knockbackProtection;
-
-    private boolean isDead;
-
-    private ArrayList<Unit> enemyUnitsInRange;
-
-    private float takenDamage;
-    private float takenKnockback;
+public class UgaBuga extends Unit {
 
     private Pixmap debug;
     private Texture debugT;
@@ -52,21 +20,11 @@ public class UgaBuga extends Actor implements Unit {
 
     @Override
     public void init(Map map, Vector2 startPosition, Direction.direction direction, Action.DomainType domain) {
-        this.map = map;
-        this.direction = direction;
-        this.domain = domain;
-        position = startPosition;
-        state = UnitState.MOVE;
-        actionTimer = 0;
-        damageFieldCreated = false;
-        isDead = false;
-        enemyUnitsInRange = new ArrayList<>();
-        damageFields = new ArrayList<>();
-
+        super.init(map, startPosition, direction, domain);
         getUnitData();
+        initHealthBar();
         initHitboxes();
         initFieldOfView();
-        initHealthBar();
 
         debug = new Pixmap(50, 20, Pixmap.Format.RGB888);
         debug.setColor(Color.PINK);
@@ -75,7 +33,6 @@ public class UgaBuga extends Actor implements Unit {
         debugT = new Texture(debug);
     }
 
-    //Get data from global class with information about unit
     private void getUnitData(){
         //TODO
         speed = 250;
@@ -95,34 +52,9 @@ public class UgaBuga extends Actor implements Unit {
 
     }
 
-    private void initHealthBar(){
-        Vector2 positionCenter = new Vector2(position.x + getWidth()/2, position.y + getHeight() + 5);
-        Vector2 size = new Vector2(getWidth() + 10, 8);
-        healthBar = new UnitHealthBar(positionCenter, size, HP, false);
-    }
-
-    private void updateHealthBarPosition(){
-        healthBar.getPosition().x = position.x + getWidth()/2;
-        healthBar.getPosition().y = position.y + getHeight() + 5;
-    }
-
-    private void changeState(UnitState newState){
-        if(state != newState){
-            state = newState;
-            actionTimer = 0;
-            damageFieldCreated = false;
-        }
-    }
-
     @Override
     public void update(float delta) {
-        actionTimer += delta;
-        updateEnemiesInRange();
-        handleTakenDamage();
-        updateState();
-        handleState(delta);
-
-        healthBar.update(HP, delta);
+        super.update(delta);
     }
 
     @Override
@@ -133,99 +65,6 @@ public class UgaBuga extends Actor implements Unit {
         updateHitboxes(positionOffset);
         updateFieldOfView(positionOffset);
         updateHealthBarPosition();
-    }
-
-    private void updateState(){
-        if(state != UnitState.DEAD)
-        {
-            if(enemyUnitsInRange.size() > 0){
-                changeState(UnitState.FIGHT);
-            }
-            else{
-                changeState(UnitState.MOVE);
-            }
-
-            if(HP <= 0){
-                changeState(UnitState.DEAD);
-            }
-        }
-
-
-    }
-
-    private void handleState(float delta){
-        switch (state){
-            case MOVE:
-                move(delta);
-                break;
-            case FIGHT:
-                fight();
-                break;
-            case DEAD:
-                death();
-                break;
-        }
-    }
-
-    private void updateEnemiesInRange(){
-        enemyUnitsInRange.clear();
-
-        MapLine landLine = map.getLines().get(Action.DomainType.LAND_UNIT);
-        for(Unit unit : landLine.units){
-            if(unit != this && unit.getDirection() != direction && unit.getState() != UnitState.DEAD){
-                float distanceX = Math.abs(unit.getPosition().x - position.x);
-                if(distanceX < 100){
-                    Rectangle enemyHitbox = unit.getHitboxes().get(0);
-                    for(Rectangle fieldOfView : fieldOfView){
-                        if(Intersector.overlaps(enemyHitbox, fieldOfView)){
-                            enemyUnitsInRange.add(unit);
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    private void initHitboxes(){
-        hitboxes = new ArrayList<>();
-        Rectangle hitbox = new Rectangle(position.x, position.y, getWidth(), getHeight());
-        hitboxes.add(hitbox);
-    }
-
-    private void updateHitboxes(float offset) {
-        for(Rectangle hitbox : hitboxes){
-            hitbox.x += offset;
-        }
-    }
-
-    private void initFieldOfView(){
-        fieldOfView = new ArrayList<>();
-        float startX = position.x + getWidth() / 2 - direction.getIntValue() * getWidth() / 2;
-        int fieldWidth = 50;
-        int fieldHeight = 30;
-        if(direction == Direction.direction.RIGHT) startX -= fieldWidth;
-        Rectangle viewHitbox = new Rectangle(startX, position.y, fieldWidth, fieldHeight);
-        fieldOfView.add(viewHitbox);
-    }
-
-    private void updateFieldOfView(float offset){
-        for(Rectangle view: fieldOfView){
-            view.x += offset;
-        }
-    }
-
-    private void death(){
-        if(actionTimer > 2){
-            isDead = true;
-        }
-    }
-
-    private void handleTakenDamage(){
-        HP -= takenDamage;
-
-        takenDamage = 0;
-        takenKnockback = 0;
     }
 
     @Override
@@ -243,15 +82,8 @@ public class UgaBuga extends Actor implements Unit {
     }
 
     @Override
-    public void damage(float damage, float knockback) {
-        takenDamage += damage;
-        takenKnockback += knockback;
-    }
-
-    @Override
     public void draw(Batch batch, float parentAlpha) {
-        batch.draw(texture, position.x, position.y);
-        healthBar.draw(batch);
+        super.draw(batch, parentAlpha);
         debugDraw(batch);
     }
 
@@ -260,56 +92,9 @@ public class UgaBuga extends Actor implements Unit {
     }
 
     @Override
-    public void act(float delta) {
-        super.act(delta);
-    }
-
-    @Override
     public void dispose() {
-        pixmap.dispose();
-        texture.dispose();
+        super.dispose();
         debug.dispose();
         debugT.dispose();
     }
-
-    @Override
-    public Actor getActor(){
-        return this;
-    }
-
-    @Override
-    public Action.DomainType getDomain() {
-        return domain;
-    }
-
-    @Override
-    public Vector2 getPosition() {
-        return position;
-    }
-
-    @Override
-    public Direction.direction getDirection() {
-        return direction;
-    }
-
-    @Override
-    public ArrayList<Rectangle> getHitboxes() {
-        return hitboxes;
-    }
-
-    @Override
-    public ArrayList<DamageField> getDamageFields() {
-        return damageFields;
-    }
-
-    @Override
-    public UnitState getState() {
-        return state;
-    }
-
-    @Override
-    public boolean isDead() {
-        return isDead;
-    }
-
 }
